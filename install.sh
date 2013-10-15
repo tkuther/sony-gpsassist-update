5ecb33128e2ac3ef6c55057985c74b56388a20f6
Thomas Kuther <gimpel@sonnenkinder.org>
2013-10-15 21:05:29 +0200

    fixlets

--- install.sh
+++ install.sh
@@ -6,4 +6,4 @@ DESTDIR="${DESTDIR:-}"
 install -Dm755 sony-gpsassist-update ${DESTDIR}${PREFIX}/bin/sony-gpsassist-update
 install -Dm644 etc/sony-gpsassist-update.cfg ${DESTDIR}${SYSCONFDIR}/sony-gpsassist-update.cfg
 install -Dm644 etc/udev/rules.d/99-sony-gpsassist-update.rules ${DESTDIR}/etc/udev/rules.d/99-sony-gpsassist-update.rules
-sed -i -e "s/@@PREFIX@@/$PREFIX/g" ${DESTDIR}${SYSCONFDIR}/udev/rules.d/99-sony-gpsassist-update.rules
+sed -i -e "s#@@PREFIX@@#${PREFIX}#" ${DESTDIR}${SYSCONFDIR}/udev/rules.d/99-sony-gpsassist-update.rules
