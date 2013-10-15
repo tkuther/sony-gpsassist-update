7ef5ecb5e2bd55b3fe98fcff8dc18e7e120b2a1c
Thomas Kuther <gimpel@sonnenkinder.org>
2013-10-15 20:37:08 +0200

    fix install.sh and script path in udev rule file

--- install.sh
+++ install.sh
@@ -7,3 +7,4 @@ DESTDIR="${DESTDIR:-}"
 install -Dm755 sony-gpsassist-update ${DESTDIR}${PREFIX}/bin/sony-gpsassist-update
 install -Dm644 etc/sony-gpsassist-update.cfg ${DESTDIR}${SYSCONFDIR}/sony-gpsassist-update.cfg
 install -Dm644 etc/udev/rules.d/99-sony-gpsassist-update.rules ${DESTDIR}${SYSCONFDIR}/udev/rules.d/99-sony-gpsassist-update.rules
+sed -i -e "s/@@PREFIX@@/$PREFIX/g" ${DESTDIR}${SYSCONFDIR}/udev/rules.d/99-sony-gpsassist-update.rules
