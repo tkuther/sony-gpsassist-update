4b4c4b1f7b9e9140963a747e13da54e9166e2d5a
Thomas Kuther <gimpel@sonnenkinder.org>
2013-10-15 20:31:21 +0200

    initial version

--- README.md
+++ README.md
@@ -2,3 +2,15 @@ sony-gpsassist-update
 =====================
 
 Automatically Update GPS Assist data on Linux
+
+Installation:
+$ ./install.sh
+
+This will
+* copy the script to /usr/bin/
+* copy the .cfg to /etc/
+* copy the udev rules.d file to /etc/udev/rules.d/
+
+Usage:
+Edit SONY_FS_UUIDS in /etc/sony-gpsassist-update.cfg
+
---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PREFIX="${PREFIX:-/usr}"
+SYSCONFDIR="${SYSCONFDIR:-/etc}"
+DESTDIR="${DESTDIR:-}"
+
+install -Dm755 sony-gpsassist-update ${DESTDIR}${PREFIX}/bin/sony-gpsassist-update
+install -Dm644 etc/sony-gpsassist-update.cfg ${DESTDIR}${SYSCONFDIR}/sony-gpsassist-update.cfg
+install -Dm644 etc/udev/rules.d/99-sony-gpsassist-update.rules ${DESTDIR}${SYSCONFDIR}/udev/rules.d/99-sony-gpsassist-update.rules
